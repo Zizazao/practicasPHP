b592cc10a8b2731cce142c7a16db90419d0aeaf7
Paco <zizamv@gmail.com>
2014-12-17 19:35:14 +0100

    alta de fichero sql

---
+++ ejerciciosqltienda.sql
@@ -0,0 +1,69 @@
+select * from articulos where ciudad = 'caceres'; 
+-- 1
+select p from envios where t = 't1';
+-- 2
+select distinct color, ciudad from componentes;
+-- 3
+select t, ciudad from articulos where ciudad like '%e%' or ciudad like '%d';
+-- 4 pattern!!!
+select p from envios where t = 't1' and c = 'c1';
+-- 5
+select tnombre from articulos 
+inner join envios 
+on articulos.t = envios.t
+where envios.p = 'p1' order by tnombre;
+-- 6
+select distinct c from envios inner join articulos on 
+articulos.t = envios.t 
+where articulos.ciudad ='madrid';
+-- 7 corregiiiir!!!!
+use tienda;
+select c from componentes where peso = (select min(peso) from componentes);
+-- 8
+select distinct table1.p from 
+(select p from envios where t = 't1') as table1
+join 
+(select p from envios where t = 't2') as table2 
+where table1.p = table2.p;
+-- 9 union, interesante, pero no sirve
+select distinct p from envios
+where p in(select p from envios where t='t1') and
+p in(select p from envios where t='t2');
+-- 9.1
+select p from componentes inner join envios on
+componentes.c = envios.c
+inner join articulos on  articulos.t = envios.t
+where componentes.color = 'rojo' and
+(articulos.ciudad = 'madrid' or articulos.ciudad = 'sevilla') ; 
+-- 10 repasar
+select c  from envios 
+where (envios.t) in (select t from articulos where ciudad = 'sevilla')
+and (envios.p) in (select p from proveedores where ciudad = 'sevilla')group by envios.c;
+-- 11 c6
+select t from envios where p = 'p1';
+-- 12 t1 t4
+select proveedores.ciudad, componentes.c, articulos.ciudad 
+from proveedores inner join componentes 
+on (proveedores.ciudad = componentes.ciudad)
+inner join articulos on (componentes.ciudad = articulos.ciudad);
+-- 13 23 filas
+select proveedores.ciudad, componentes.c, articulos.ciudad 
+from proveedores inner join componentes 
+on (proveedores.ciudad = componentes.ciudad)
+inner join articulos on 
+(componentes.ciudad = articulos.ciudad);
+-- 14 15 filas
+select count(c), count(distinct(t)), sum(cantidad) from envios where p = 'p2';
+-- 15
+select distinct c, t, cantidad from envios as t1
+inner join (select sum(cantidad) from envios as t1) as t2 group by c, t;
+-- 16 20 filas
+select t1.t from articulos as t1 inner join articulos on (t1.ciudad <> 'madrid')
+inner join proveedores on (t1.ciudad <> proveedores.ciudad)
+group by t1.t;
+-- 17 todos menos t6
+select p from envios;
+-- 18 p1 p2 p3 p4 p5
+-- 19 todos menos t7
+-- 20 p1 p2 p5
+
